e2963d64462008e63677dc1d0514481fb03a7d1f
Josiah Campbell <9521010+jocmp@users.noreply.github.com>
2025-01-01 16:41:26 -0600

    feat: Add custom parser - techcrunch.com

---
+++ src/extractors/custom/techcrunch.com/index.test.js
@@ -0,0 +1,77 @@
+import assert from 'assert';
+import URL from 'url';
+import cheerio from 'cheerio';
+
+import Parser from 'mercury';
+import getExtractor from 'extractors/get-extractor';
+import { excerptContent } from 'utils/text';
+
+const fs = require('fs');
+
+describe('TechcrunchComExtractor', () => {
+  describe('initial test case', () => {
+    let result;
+    let url;
+    beforeAll(() => {
+      url =
+        'https://techcrunch.com/2024/12/23/halides-next-version-will-come-with-new-film-filters-hdr/';
+      const html = fs.readFileSync(
+        './fixtures/techcrunch.com/1735770877170.html'
+      );
+      result = Parser.parse(url, { html, fallback: false });
+    });
+
+    it('is selected properly', () => {
+      const extractor = getExtractor(url);
+      assert.equal(extractor.domain, URL.parse(url).hostname);
+    });
+
+    it('returns the title', async () => {
+      const { title } = await result;
+
+      assert.equal(
+        title,
+        `Halide's next version will come with new film filters, HDR`
+      );
+    });
+
+    it('returns the author', async () => {
+      const { author } = await result;
+
+      assert.equal(author, 'Ivan Mehta');
+    });
+
+    it('returns the date_published', async () => {
+      const { date_published } = await result;
+
+      assert.equal(date_published, `2024-12-24T07:27:20.000Z`);
+    });
+
+    it('returns the lead_image_url', async () => {
+      const { lead_image_url } = await result;
+
+      assert.equal(
+        lead_image_url,
+        `https://techcrunch.com/wp-content/uploads/2024/12/Halide-Mark-3.jpg?resize=1200,712`
+      );
+    });
+
+    it('returns the content', async () => {
+      const { content } = await result;
+
+      const $ = cheerio.load(content || '');
+
+      const first13 = excerptContent(
+        $('*')
+          .first()
+          .text(),
+        13
+      );
+
+      assert.equal(
+        first13,
+        'Lux, which makes the iPhone camera app Halide, published a roadmap on Monday'
+      );
+    });
+  });
+});
