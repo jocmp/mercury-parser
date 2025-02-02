f183a63b2d9429ba861eb3600863a0d3ccef2797
Josiah Campbell <9521010+jocmp@users.noreply.github.com>
2025-02-01 21:46:28 -0600

    feat: Add custom parser for heise.de (#48)

---
+++ src/extractors/custom/www.heise.de/index.js
@@ -0,0 +1,33 @@
+export const WwwHeiseDeExtractor = {
+  domain: 'www.heise.de',
+
+  title: {
+    selectors: [['meta[name="og:title"]', 'value']],
+  },
+
+  author: {
+    selectors: [['meta[name="author"]', 'value']],
+  },
+
+  date_published: {
+    selectors: [['meta[name="date"]', 'value']],
+  },
+
+  dek: {
+    selectors: ['.a-article-header__lead'],
+  },
+
+  lead_image_url: {
+    selectors: [['meta[name="og:image"]', 'value']],
+  },
+
+  content: {
+    selectors: ['.article-layout__content'],
+
+    transforms: {
+      h3: $node => $node.attr('class', 'mercury-parser-keep'),
+    },
+
+    clean: [],
+  },
+};
