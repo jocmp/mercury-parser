9214f730c9d962e415ea6af12ba7a844b9d9a77d
Josiah Campbell <9521010+jocmp@users.noreply.github.com>
2025-02-04 19:48:45 -0600

    feat: Add custom parser for polityka.se.pl

--- src/extractors/custom/polityka.se.pl/index.js
+++ src/extractors/custom/polityka.se.pl/index.js
@@ -21,7 +21,9 @@ export const PolitykaSePlExtractor = {
   content: {
     selectors: ['article'],
 
-    transforms: {},
+    transforms: {
+      h2: node => node.attr('class', 'mercury-parser-keep'),
+    },
 
     clean: [
       '.article__author__croppimg', // author photo
