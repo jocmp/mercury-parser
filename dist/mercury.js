b2b2f6ad995bf38bcb84e85008fdf92d303b2f6f
jocmp <9521010+jocmp@users.noreply.github.com>
2025-02-26 03:18:10 +0000

    bump version v2.3.7 -> v2.4.0

--- dist/mercury.js
+++ dist/mercury.js
@@ -6606,6 +6606,62 @@ var BialystokSePlExtractor = _objectSpread({}, WwwSePlExtractor, {
   domain: 'bialystok.se.pl'
 });
 
+var WwwLebensmittelwarnungDeExtractor = {
+  domain: 'www.lebensmittelwarnung.de',
+  title: {
+    selectors: ['.lmw-intro__heading', 'title']
+  },
+  date_published: {
+    selectors: [['.lmw-intro__meta > time', 'datetime']],
+    timezone: 'Europe/Berlin'
+  },
+  lead_image_url: {
+    selectors: [['meta[name="og:image"]', 'value']]
+  },
+  content: {
+    selectors: ['main'],
+    transforms: {
+      h2: function h2(node) {
+        var button = node.find('button');
+
+        if (node.find('button').length > 0) {
+          node.find('.lmw-section__toggle-icon').remove();
+          node.text(button.text().trim());
+        }
+
+        node.attr('class', 'mercury-parser-keep');
+      },
+      ul: function ul($node) {
+        $node.attr('class', 'mercury-parser-keep');
+      },
+      '.lmw-bodytext': function lmwBodytext(node) {
+        // Kontakt Information
+        node.attr('class', 'mercury-parser-keep');
+      },
+      '.lmw-description-list__item': function lmwDescriptionList__item(node) {
+        node.attr('class', 'mercury-parser-keep');
+      }
+    },
+    clean: []
+  }
+};
+
+var WwwQbitaiComExtractor = {
+  domain: 'www.qbitai.com',
+  title: {
+    selectors: ['title', 'h1']
+  },
+  content: {
+    selectors: ['.article'],
+    transforms: {
+      '.zhaiyao': function zhaiyao(node) {
+        return node.attr('class', 'mercury-parser-keep');
+      }
+    },
+    clean: ['.article_info']
+  }
+};
+
 
 
 var CustomExtractors = /*#__PURE__*/Object.freeze({
@@ -6776,7 +6832,9 @@ var CustomExtractors = /*#__PURE__*/Object.freeze({
   LodzSePlExtractor: LodzSePlExtractor,
   WroclawSePlExtractor: WroclawSePlExtractor,
   LublinSePlExtractor: LublinSePlExtractor,
-  BialystokSePlExtractor: BialystokSePlExtractor
+  BialystokSePlExtractor: BialystokSePlExtractor,
+  WwwLebensmittelwarnungDeExtractor: WwwLebensmittelwarnungDeExtractor,
+  WwwQbitaiComExtractor: WwwQbitaiComExtractor
 });
 
 var Extractors = _Object$keys(CustomExtractors).reduce(function (acc, key) {
