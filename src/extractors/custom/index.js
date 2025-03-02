33fb467d9cffe3e13b58b4c97f24c71183dac745
Josiah Campbell <9521010+jocmp@users.noreply.github.com>
2025-03-02 15:34:18 -0600

    feat: Add custom parser for 9to5google.com

--- src/extractors/custom/index.js
+++ src/extractors/custom/index.js
@@ -171,3 +171,4 @@ export * from './www.qbitai.com';
 export * from './economictimes.indiatimes.com';
 export * from './factorio.com';
 export * from './www.tagesschau.de';
+export * from './9to5google.com';
