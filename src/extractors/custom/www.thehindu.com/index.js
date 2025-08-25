dc7ef23cc6db197e3969d3e89a83b9b877b4e13d
mahendran <mahendran@noreply.mahendran.my>
2025-08-25 12:57:43 +0530

    lint

--- src/extractors/custom/www.thehindu.com/index.js
+++ src/extractors/custom/www.thehindu.com/index.js
@@ -35,13 +35,6 @@ export const WwwThehinduComExtractor = {
   content: {
     selectors: ['div[itemprop="articleBody"]'],
 
-    // Is there anything in the content you selected that needs transformed
-    // before it's consumable content? E.g., unusual lazy loaded images
-    transforms: {},
-
-    // Is there anything that is in the result that shouldn't be?
-    // The clean selectors will remove anything that matches from
-    // the result
     clean: ['div.article-ad', 'div[data-ga-label="AlsoReadArticle"]'],
   },
 };
