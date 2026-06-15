f343c416bfa896929ae5cde099aae6a7025a03a5
Josiah Campbell <9521010+jocmp@users.noreply.github.com>
2026-06-14 20:17:45 -0500

    feat: Add motorsport.com custom parser (#175)

---
+++ src/extractors/custom/www.motorsport.com/index.js
@@ -0,0 +1,36 @@
+export const WwwMotorsportComExtractor = {
+  domain: 'www.motorsport.com',
+
+  title: {
+    selectors: [['meta[name="og:title"]', 'value']],
+  },
+
+  author: {
+    selectors: ['.msnt-author-toolbar a[href*="/info/about-us/"]'],
+  },
+
+  date_published: {
+    selectors: [['meta[name="datePublished"]', 'value']],
+  },
+
+  dek: {
+    selectors: ['h2.text-article-description'],
+  },
+
+  lead_image_url: {
+    selectors: [['meta[name="og:image"]', 'value']],
+  },
+
+  content: {
+    selectors: ['.ms-article-content'],
+    transforms: {
+      h2: node => node.attr('class', 'mercury-parser-keep'),
+    },
+    clean: [
+      '.relatedContent',
+      '.ms-apb',
+      '.ms-ap-native',
+      '.outstream_partner',
+    ],
+  },
+};
